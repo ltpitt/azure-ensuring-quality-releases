1f83f586be4c62d531eb41b4836b02bf96f7697b
Davide Nastri <d.nastri@gmail.com>
2021-09-16 00:03:14 +0200

    feat: Added create_storage_account_in_azure.sh script

---
+++ create_storage_account_in_azure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RESOURCE_GROUP_NAME=tstate
+STORAGE_ACCOUNT_NAME=tstate$RANDOM
+CONTAINER_NAME=tstate
+LOCATION=eastus2
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+# Get storage account key
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+
+echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
+echo "container_name: $CONTAINER_NAME"
+echo "access_key: $ACCOUNT_KEY"
+
+export ARM_ACCESS_KEY=$ACCOUNT_KEY
+
+# Store Azure Storage account in Azure Key Vault
+export ARM_ACCESS_KEY=$(az keyvault secret show --name terraform-backend-key --vault-name myKeyVault --query value -o tsv)
