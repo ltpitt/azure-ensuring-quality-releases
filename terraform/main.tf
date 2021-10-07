f85c90c3ab1defd04d865c9c52a9ff27962eea60
Davide Nastri <d.nastri@gmail.com>
2021-10-07 23:05:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,7 @@ provider "azurerm" {
 }
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.50.0"
-    }
+    azurerm = "2.52.0"
   }
   backend "azurerm" {
     storage_account_name = "tstate12016"
