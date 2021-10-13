c7b9160d482fcdd74f48cbad8766f44b1fab3e31
Davide Nastri <d.nastri@gmail.com>
2021-10-14 01:15:25 +0200

    Update selenium-tests.py

--- automatedtesting/selenium/selenium-tests.py
+++ automatedtesting/selenium/selenium-tests.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
-import datetime
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 
 
-def date():
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
 def login(user, password):
-    print(date() + ' Begin of Selenium Test.')
     options = ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=options)
-    print(date() + ' Logging in...')
     driver.get('https://www.saucedemo.com/')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
@@ -25,8 +17,7 @@ def login(user, password):
 
     product_label = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span').get_attribute('innerHTML')
     assert "Products" in product_label
-    print(date() + " User " + user + " logged in successfully!")
-
+    print("User " + user + " logged in successfully!")
     product_list = driver.find_elements_by_class_name("inventory_item")
 
     for product in product_list:
